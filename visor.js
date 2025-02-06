3650daa344c27807f4df99d7172629f97e68e746
Janespot <janejunell777@gmail.com>
2025-02-06 12:24:58 +0300

    Tensorflow js Visor project

---
+++ visor.js
@@ -0,0 +1,18 @@
+function Draw(id) {
+    const surface = document.getElementById("draw");
+    const series = ['First', 'Second'];
+
+    const serie1 = [];
+    const serie2 = [];
+
+    for (let i = 0; i < 100; i++) {
+        serie1[i] = { x: i, y: Math.random() * 100 };
+        serie2[i] = { x: i, y: Math.random() * 100 };
+    }
+
+    const data = { values: [serie1, serie2], series};
+
+    tfvis.render.scatterplot(surface, data);
+}
+
+Draw();
